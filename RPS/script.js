87c0873bf127d3532adb2a776d3ed1e50c065fe6
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-09-26 09:31:19 -0500

    logic finished

--- RPS/script.js
+++ RPS/script.js
@@ -1,36 +1,55 @@
 let botPlay, wins = 0, botwin = 0;
+
 function play(id) {
-        if (botPlay !== id) {
-            switch (id) {
-                case "r":
-                    if(botPlay === "s"){
-                        document.getElementById("text").innerHTML = "Oh No! Try again."
-                        botwin++
-                    }else{
-                        wins++
-                        document.getElementById("text").innerHTML = "Great Job! Your current score is " + wins + "."
-                    }
-                    break
-                case "s":
-                    if(botPlay === "p"){
-                        document.getElementById("text").innerHTML = "Oh No! Try again."
-                        botwin++
-                    }else{
-                        wins++
-                        document.getElementById("text").innerHTML = "Great Job! Your current score is " + wins + "."
-                    }
-                    break
-                case "p":
-                    if(botPlay === "r"){
-                        document.getElementById("text").innerHTML = "Oh No! Try again."
-                        botwin++
-                    }else{
-                        wins++
-                        document.getElementById("text").innerHTML = "Great Job! Your current score is " + wins + "."
-                    }
-                    break
-            }
-        }else{
-            document.getElementById("text").innerHTML = "Tie!"
-        }
+    switch (gennum()) {
+        case 0:
+            botPlay = "r"
+            break
+        case 1:
+            botPlay = "s"
+            break
+        case 2:
+            botPlay = "p"
+            break
+        default:
+            console.log("error")
+
     }
+    if (botPlay !== id) {
+        switch (id) {
+            case "r":
+                if (botPlay !== "s") {
+                    document.getElementById("text").innerHTML = "Oh No! Try again."
+                    botwin++
+                } else {
+                    wins++
+                    document.getElementById("text").innerHTML = "Great Job! Your current score is " + wins + "."
+                }
+                break
+            case "s":
+                if (botPlay !== "p") {
+                    document.getElementById("text").innerHTML = "Oh No! Try again."
+                    botwin++
+                } else {
+                    wins++
+                    document.getElementById("text").innerHTML = "Great Job! Your current score is " + wins + "."
+                }
+                break
+            case "p":
+                if (botPlay !== "r") {
+                    document.getElementById("text").innerHTML = "Oh No! Try again."
+                    botwin++
+                } else {
+                    wins++
+                    document.getElementById("text").innerHTML = "Great Job! Your current score is " + wins + "."
+                }
+                break
+        }
+    } else {
+        document.getElementById("text").innerHTML = "Tie!"
+    }
+}
+
+function gennum() {
+    return Math.round(Math.random() * 2);
+}
