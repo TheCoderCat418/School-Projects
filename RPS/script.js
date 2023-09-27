33d6ef2b36f6e0c0d8c0fc96ab71272e895eb365
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-09-27 09:59:30 -0500

    idkkkkkkkkk

--- RPS/script.js
+++ RPS/script.js
@@ -1,6 +1,6 @@
-let botPlay, wins = 0, botwin = 0;
+let botPlay, wins = 0, botwin = 0; rounds = prompt("How many rounds")-1
 
-function play(id) {
+function getBotPlay() {
     //document.getElementById()
     switch (gennum()) {
         case 0:
@@ -9,7 +9,6 @@ function play(id) {
             break
         case 1:
             document.getElementById("imga").src ="https://i.redd.it/zkf5jfp0ul441.png"
-
             botPlay = "s"
             break
         case 2:
@@ -19,10 +18,22 @@ function play(id) {
         default:
             console.log("error")
 
-    }
+    }}
+    function play(id){
+        if (rounds<0){
+            document.getElementById("text").innerHTML = "All done! "
+            if(botwin>wins){
+                document.getElementById("text").innerHTML += "Bot wins! " + botwin +" - "+wins
+            }else{
+                document.getElementById("text").innerHTML += "You win! " + wins +" - "+botwin
+            }
+            return
+        }
+        getBotPlay()
     if (botPlay !== id) {
         switch (id) {
             case "r":
+                document.getElementById("imgb").src = "https://i.etsystatic.com/30512802/r/il/231676/4262629253/il_fullxfull.4262629253_7lky.jpg"
                 if (botPlay !== "s") {
                     document.getElementById("text").innerHTML = "Oh No! Try again."
                     botwin++
@@ -32,6 +43,7 @@ function play(id) {
                 }
                 break
             case "s":
+                document.getElementById("imgb").src ="https://i.redd.it/zkf5jfp0ul441.png"
                 if (botPlay !== "p") {
                     document.getElementById("text").innerHTML = "Oh No! Try again."
                     botwin++
@@ -41,6 +53,7 @@ function play(id) {
                 }
                 break
             case "p":
+                document.getElementById("imgb").src ="https://i.imgflip.com/5abvnc.jpg"
                 if (botPlay !== "r") {
                     document.getElementById("text").innerHTML = "Oh No! Try again."
                     botwin++
@@ -52,7 +65,21 @@ function play(id) {
         }
     } else {
         document.getElementById("text").innerHTML = "Tie!"
+        rounds++
+        switch (id) {
+            case "r":
+                document.getElementById("imgb").src = "https://i.etsystatic.com/30512802/r/il/231676/4262629253/il_fullxfull.4262629253_7lky.jpg"
+                break
+            case "s":
+                document.getElementById("imgb").src ="https://i.redd.it/zkf5jfp0ul441.png"
+                break
+            case "p":
+                document.getElementById("imgb").src ="https://i.imgflip.com/5abvnc.jpg"
+                break
+        }
     }
+    rounds--
+
 }
 
 function gennum() {
