0ca62c2870cb3ae11b3ad759718f371771e00d71
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-10-20 09:59:27 -0500

    C

--- untitled/script.js
+++ untitled/script.js
@@ -1,4 +1,5 @@
-let movies = ["spacecamp"], imgMovies = ["imgs/spacecamp.jpg"], smovie = "", points = 0
+let movies = ["spacecamp", "wall-e","indianajones", "starwars","missionimpossible", "charliebrownchristmas", "elf", "rudolphtherednosedreindeer", "homealone", "polorexpress"]
+let imgMovies = ["imgs/spacecamp.jpg", "imgs/wall-e.jpg", "imgs/inde.jpg", "imgs/sw.jpg", "imgs/mi1.jpg", "imgs/cbc.jpg", "imgs/elf.jpg", "imgs/rtrnr.jpg", "imgs/ha.jpg", "imgs/pe.jpg"], smovie = "", points = 0
 
 function pickRandom(){
     let rnum = Math.floor(Math.random()*movies.length)
@@ -6,12 +7,10 @@ function pickRandom(){
     document.getElementById("mimg").src = imgMovies[rnum]
 }
 
-
-
-
-
 function subed(){
     if(smovie === document.getElementById("textbx").value){
-        points++
+        points++;
     }
+    document.getElementById("textbx").value = ""
+    pickRandom()
 }
