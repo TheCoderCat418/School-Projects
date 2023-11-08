45ac2306ac2c2bc838fe903c9ffc8df55a87ef8d
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-11-08 09:59:30 -0600

    DVD LOGO

--- misc/smth/day 2/script.js
+++ misc/smth/day 2/script.js
@@ -1,19 +1,35 @@
 let a;
+let pig = new Image()
+pig.src = "r/DVD_logo.svg.png"
+
 function ani(){
     let canvas = document.getElementById("caned")
     let ctx = canvas.getContext("2d")
-    ctx.fillStyle = Math.floor(Math.random()*16777215).toString(16);
+    ctx.drawImage(pig, 50,50,200,100)
     aacanimate()
 }
-let i = 0
+let i = 50, max = 5, y = 50,may = 5
 function aacanimate(){
      a = requestAnimationFrame(aacanimate)
     let canvas = document.getElementById("caned")
     let ctx = canvas.getContext("2d")
     drawBackground()
-    ctx.fillStyle = "#" + Math.floor(Math.random()*16777215).toString(16);
-    drawRectangle(i,50, 200,200)
-    i+=1
+    if(i>2000){
+        max=max*-1
+    }
+    if(y>900){
+        may=may*-1
+    }
+    if(i<0){
+        max=max*-1
+    }
+    if(y<0){
+        may=may*-1
+    }
+    y+=may
+    i+=max
+    ctx.drawImage(pig, i,y,200,100)
+
 
 }
 function drawRectangle(x,y,w,h){
@@ -25,7 +41,7 @@ function drawBackground(){
     let canvas = document.getElementById("caned")
     let ctx = canvas.getContext("2d")
     ctx.fillStyle = "#FFFFFF"
-    ctx.fillRect(0, 0, 2000, 2000)
+    ctx.fillRect(0, 0, 20000, 20000)
 }
 function STOP(){
     cancelAnimationFrame(a)
