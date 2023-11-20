5dbeb1d8486c9b2873d800f0148febd9e9ba23bb
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-11-20 09:59:57 -0600

    f

--- misc/smth/aple/apple.js
+++ misc/smth/aple/apple.js
@@ -1,6 +1,6 @@
 let a,spwanTime=500;
-let bask, apples = [], appscollected = 0, famps=0
-
+let bask, apples = [], appscollected = 0, famps=0, hearts = []
+let bullit;
 function startAnimation(){
     animate()
 }
@@ -9,33 +9,40 @@ function stopAnimation(){
 }
 
 function animate() {
-    a = requestAnimationFrame(animate)
-    drawBackground()
-    drawTree()
-    drawBasket()
-    moveApple()
-    drawApple()
-    console.log(spwanTime, appscollected, spwanTime/2-(appscollected/2))
-    for(let i = 0; i<apples.length;i++){
-        if(checkCollisionsOfBasket(i)){
-            i=0
+    if(famps !== 3) {
+        a = requestAnimationFrame(animate)
+        drawBackground()
+        drawTree()
+        drawBasket()
+        moveApple()
+        drawApple()
+        drawHears()
+        drawBullet()
+        console.log(spwanTime, appscollected, spwanTime / 2 - (appscollected / 2))
+        for (let i = 0; i < apples.length; i++) {
+            if (checkCollisionsOfBasket(i)) {
+                i = 0
+            }
         }
-    }
-    if(famps === 5) {
+    }else{
+        let ctx = document.getElementById("myCanvas").getContext("2d");
+        let ings = new Image()
+        ings.src = "resources/aznrmij72wu4ma9mcads.jpg"
+        ctx.drawImage(ings, 0,0,500,500)
         endGame()
     }
 }
 let aaa= 3
 function endGame(){
+
     stopAnimation()
-    let ctx = document.getElementById("myCanvas").getContext("2d");
-    ctx.drawImage(createImage("resources/aznrmij72wu4ma9mcads.jpg"), 0,0,500,500)
 }
 function initialize(){
     drawBackground()
     drawTree()
     createBasket()
     makeApple()
+    createHearts()
 }
 
 let tree = new Image()
@@ -52,7 +59,18 @@ function drawTree(){
     let ctx = document.getElementById("myCanvas").getContext("2d");
     ctx.drawImage(tree, 100, 0, 300, 500 )
 }
-
+function createBuller(){
+    bullit = createImage("resources/Untitled.jpg", bask.left, 500, -10)
+}
+function drawBullet(){
+    bullit.top += bullit.velo
+    let ctx = document.getElementById("myCanvas").getContext("2d");
+    ctx.drawImage(bullit, bullit.left, bullit.top, 50, 50)
+    if(bullit.top < -50){
+        bullit = null
+        canFire =true
+    }
+}
 function drawBasket(){
     let ctx = document.getElementById("myCanvas").getContext("2d");
     ctx.drawImage(bask, bask.left, bask.top, 50, 50)
@@ -60,11 +78,27 @@ function drawBasket(){
 function createBasket(){
     bask = createImage("resources/basket.png", 250, 425, 0)
 }
+function createHearts(){
+    hearts[0] = createImage("resources/R.png", 10, 10, 0, true);
+    hearts[1] = createImage("resources/R.png", 60, 10, 0, true);
+    hearts[2] = createImage("resources/R.png", 110, 10, 0, true);
+}
+function drawHears(){
+    let ctx = document.getElementById("myCanvas").getContext("2d");
+    for(let i = 0; i<hearts.length;i++){
+        if(hearts[i].visi){
+            ctx.drawImage(hearts[i], hearts[i].left, hearts[i].top, 50, 50)
+        }
+    }
+
+
+
+}
 function makeApple(){
     apples[apples.length] = createImage("resources/apple.png",Math.floor(Math.random()*500), -50, 0.5)
     setTimeout(()=>{
         makeApple()
-    }, 500)
+    }, 1000)
 }
 function moveApple(){
     for(let i = 0; i<apples.length; i++){
@@ -78,22 +112,23 @@ function drawApple(){
         if(apples[i].top > 500){
             apples.splice(i,1);
             famps++
+            hearts[famps-1].visi = false
         }
     }
 }
 
 
-let createImage = function(src, xcoord, ycoord, velo){
+let createImage = function(src, xcoord, ycoord, velo, visi){
     let img = new Image()
     img.src = src;
     img.left = xcoord;
     img.top = ycoord;
     img.velo = velo;
-
+    img.visi = visi
     return img;
 }
 
-
+let canFire = true
 
 $(document).keydown((event) => {  //jQuery code to recognize a keydown event
     let keycode = (event.keyCode ? event.keyCode : event.which);
@@ -114,6 +149,12 @@ $(document).keydown((event) => {  //jQuery code to recognize a keydown event
     }
     if(keycode== 83){
         //linkY = linkY +5
+    }
+    if(keycode == 32){
+        if(canFire) {
+            createBuller()
+            canFire = false
+        }
     }
 
 });
