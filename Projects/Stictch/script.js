3ab38b3bd629ca90d33e13cd601d903b9d87c269
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-11-29 09:57:18 -0600

    Starting lazer

--- Projects/Stictch/script.js
+++ Projects/Stictch/script.js
@@ -1,42 +1,71 @@
-let animationFrame, canvas, player, evilShips = []
+let animationFrame, canvas, player, evilShips = [], movingUp = false, goodLazer
 
 
-function init(){
+function init() {
     canvas = document.getElementById("caned").getContext("2d")
     player = createImage("rec/The_Red_One.png", 0, 375, 150, 75)
     makeEnimies()
     animateLoop()
 }
-function animateLoop(){
+
+function animateLoop() {
     animationFrame = requestAnimationFrame(animateLoop);
     drawBackground()
     drawCharacter()
+    moveEnemies()
     drawEnemies()
 
 }
-function drawBackground(){
- canvas.fillRect(0,0, 2000, 1250)
+
+function drawBackground() {
+    canvas.fillRect(0, 0, 2000, 1250)
 }
-function drawCharacter(){
+
+function drawCharacter() {
     canvas.fillStyle = "#2d0470"
     canvas.drawImage(player, player.xval, player.yval, player.sizex, player.sizey)
 }
-function drawEnemies(){
-for(let j = 0; j<evilShips.length; j++){
-    for(let i = 0; i<evilShips[j].length; i++){
-        canvas.drawImage(evilShips[j][i], evilShips[j][i].xval, evilShips[j][i].yval, evilShips[j][i].sizex, evilShips[j][i].sizey)
+
+function moveEnemies() {
+    if(evilShips[evilShips.length-1][evilShips[evilShips.length-1].length-1].yval + evilShips[evilShips.length-1][evilShips[evilShips.length-1].length-1].sizey > 750){
+        movingUp = true
+    }else if(evilShips[evilShips.length-1][0].yval < 0){
+        movingUp = false
+    }
+    for (let j = 0; j < evilShips.length; j++) {
+
+        for (let i = 0; i < evilShips[j].length; i++) {
+            if (movingUp) {
+                evilShips[j][i].yval -= evilShips[j][i].velo
+            } else {
+                evilShips[j][i].yval += evilShips[j][i].velo
+            }
+        }
+
+
     }
 }
-}
-function makeEnimies(){
-    for(let j = 0; j<2;j++) {
-        evilShips[j] = []
-        for (let i = 0; i < 5; i++) { //OFFSET + NUMINROW * (SIZE + PADDING)
-            evilShips[j][i] = createImage("rec/Police_Cruiser.png", 1000+j * (150+50),  50+i * (75+50), 150, 75)
+
+function drawEnemies() {
+    for (let j = 0; j < evilShips.length; j++) {
+        for (let i = 0; i < evilShips[j].length; i++) {
+            canvas.drawImage(evilShips[j][i], evilShips[j][i].xval, evilShips[j][i].yval, evilShips[j][i].sizex, evilShips[j][i].sizey)
         }
     }
 }
-let createImage = function(src, xcoord, ycoord, sizex, sizey,  velo, visi){
+
+function makeEnimies() {
+    for (let j = 0; j < 2; j++) {
+        evilShips[j] = []
+        for (let i = 0; i < 5; i++) { //OFFSET + NUMINROW * (SIZE + PADDING)
+            evilShips[j][i] = createImage("rec/Police_Cruiser.png", 1000 + j * (150 + 50), 50 + i * (75 + 50), 150, 75, 2)
+        }
+    }
+}
+function shootGoodLaser(){
+
+}
+let createImage = function (src, xcoord, ycoord, sizex, sizey, velo, visi) {
     let img = new Image()
     img.src = src;
     img.xval = xcoord;
@@ -52,23 +81,23 @@ let createImage = function(src, xcoord, ycoord, sizex, sizey,  velo, visi){
 $(document).keydown((event) => {
     let keycode = (event.keyCode ? event.keyCode : event.which);
 
-    if(keycode == 87){
-        if(player.yval> 0) {
+    if (keycode == 87) {
+        if (player.yval > 0) {
             player.yval -= 5
-        }else {
+        } else {
             console.log("Fail, player.yval> -750")
         }
     }
-    if(keycode== 83) {
+    if (keycode == 83) {
         if (player.yval < 750 - player.sizey) {
             player.yval += 5
-        }else {
+        } else {
             console.log("Fail, player.yval > 0")
         }
     }
-console.log(keycode, player.yval)
+    console.log(keycode)
 
-    if(keycode == 32){
+    if (keycode == 32) {
 
     }
 
