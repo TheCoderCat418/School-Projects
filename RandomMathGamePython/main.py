66e998cd98623cf2fbc02adb03e4398ff34cdc9a
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-01-22 10:00:09 -0600

    s

--- RandomMathGamePython/main.py
+++ RandomMathGamePython/main.py
@@ -35,20 +35,23 @@ selectedDifficulty = "E"
 #             selectedDifficulty = "H"
 #         case _:
 #             print("We were unable to determine the difficulty you wanted. Please try again. \n\n")
-continuousMode = True
-while selectedDifficulty == "":
-    diff = input("Please select difficulty.\n \nAvailable difficulty: Easy, Hard\n>")
+continuousMode = -1
+while continuousMode == -1:
+    diff = input("Would you like continuousMode to be enabled? This will go on until you are done. (Y/n)")
     match diff.upper():
-        case "EASY":
-            selectedDifficulty = "E"
-        case "HARD":
-            selectedDifficulty = "H"
+        case "Y":
+            continuousMode = 1
+        case "N":
+            continuousMode = 0
         case _:
-            print("We were unable to determine the difficulty you wanted. Please try again. \n\n")
+            print("Sorry we didn't get that. Please try again. \n\n")
 
 # Assemble Questions
 while True:
     amtOfQuestions = 5
+    if continuousMode == 1:
+        amtOfQuestions = 1000
+        selectedDifficulty == "M"
     questions = []
 
     for x in range(amtOfQuestions):
@@ -57,7 +60,7 @@ while True:
             multiplier = 5
         elif selectedDifficulty == "M":
             multiplier = 2
-        match random.randint(0,2):
+        match random.randint(0, 2):
             case 0:
                 selectedMode = "ADD"
             case 1:
@@ -67,27 +70,32 @@ while True:
         if selectedMode == "ADD":
             num1, num2 = random.randint(0, 10 * multiplier), random.randint(0, 10 * multiplier)
             questions.append({"q": "What is " + str(num1) + " + " + str(num2) + " ? ", "a": num1 + num2, "s": False,
-                              "o": "ADD"})
+                              "o": selectedMode})
         elif selectedMode == "SUB":
             num1, num2 = random.randint(0, 10 * multiplier), random.randint(0, 10 * multiplier)
             questions.append({"q": "What is " + str(num1) + " - " + str(num2) + " ? ", "a": num1 + num2, "s": False,
                               "o": selectedMode})
         elif selectedMode == "MULTI":
-            num1, num2 = random.randint(0, 10 * math.floor(multiplier-0.6*multiplier)), random.randint(0, 10 * math.floor(multiplier-0.6*multiplier))
+            num1, num2 = (random.randint(0, 10 * math.floor(multiplier - 0.3 * multiplier)),
+                          random.randint(0, 10 * math.floor(multiplier - 0.3 * multiplier)))
             questions.append({"q": "What is " + str(num1) + " * " + str(num2) + " ? ", "a": num1 + num2, "s": False,
                               "o": selectedMode})
 
-
-
+    print("--- WHEN YOU ARE DONE, TYPE 'DONE' IN ANY QUESTION PROMPT ---")
     for i in range(len(questions)):
         attempt = 3
+        if continuousMode == -2:
+            break
         while True:
             try:
                 if attempt == 0:
                     print("Looks like you don't know this one. Lets move on. \n")
                     break
-                anw = int(input(questions[i]["q"]))
-                if anw == questions[i]["a"]:
+                anw = input(questions[i]["q"])
+                if anw.upper() == "DONE":
+                    continuousMode = -2
+                    break
+                elif int(anw) == questions[i]["a"]:
                     questions[i]["s"] = True
                     break
                 else:
@@ -102,6 +110,9 @@ while True:
             correct += 1
         if i == len(questions) - 1:
             print("DONE! \n" + str(correct / amtOfQuestions * 100) + "%")
+
+    if continuousMode == -2 or continuousMode == 1:
+        break
 
     if correct / amtOfQuestions * 100 >= 80.0 and selectedDifficulty == "E":
         selectedDifficulty = "M"
