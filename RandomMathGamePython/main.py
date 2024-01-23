6f8a9f21ce3f1e6e616ed0cc0c288d16b2c41972
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-01-23 10:00:10 -0600

    A

--- RandomMathGamePython/main.py
+++ RandomMathGamePython/main.py
@@ -1,41 +1,24 @@
-import math
 import random
-import time
 
-# Mode
 selectedMode = ""
-# while selectedMode == "":
-#     mode = input("Please select mode of operation.\n \nAvailable operations: Addition (add), Subtraction (sub), "
-#                  "Multiplication (mutli)\n>")
-#     match mode.upper():
-#         case "ADDITION":
-#             selectedMode = "ADD"
-#         case "SUBTRACTION":
-#             selectedMode = "SUB"
-#         case "Multiplication":
-#             selectedMode = "MULTI"
-#         case "ADD":
-#             selectedMode = "ADD"
-#         case "SUB":
-#             selectedMode = "SUB"
-#         case "MULTI":
-#             selectedMode = "MULTI"
-#         case _:
-#             selectedMode = ""
-#             print("We were unable to determine the operation you wanted. Please try again. \n")
-
-# Difficulty
 selectedDifficulty = "E"
-# while selectedDifficulty == "":
-#     diff = input("Please select difficulty.\n \nAvailable difficulty: Easy, Hard\n>")
-#     match diff.upper():
-#         case "EASY":
-#             selectedDifficulty = "E"
-#         case "HARD":
-#             selectedDifficulty = "H"
-#         case _:
-#             print("We were unable to determine the difficulty you wanted. Please try again. \n\n")
 continuousMode = -1
+qAnswered = 0
+qCorrect = 0
+p2Mode: bool = False
+a = False
+while not a:
+    diff = input("Do you want to play with a friend? (Y/n)")
+    match diff.upper():
+        case "Y":
+            a = True
+            p2Mode = True
+            continuousMode = 1
+        case "N":
+            a = True
+            p2Mode = False
+        case _:
+            print("Sorry we didn't get that. Please try again. \n\n")
 while continuousMode == -1:
     diff = input("Would you like continuousMode to be enabled? This will go on until you are done. (Y/n)")
     match diff.upper():
@@ -52,68 +35,117 @@ while True:
     if continuousMode == 1:
         amtOfQuestions = 1000
         selectedDifficulty == "M"
+        print("--- WHEN YOU ARE DONE, TYPE 'DONE' IN ANY QUESTION PROMPT ---")
     questions = []
 
+    # Question factory
     for x in range(amtOfQuestions):
         multiplier = 1
-        if selectedDifficulty == "H":
+        if selectedDifficulty == "H" or qCorrect > 20:
             multiplier = 5
         elif selectedDifficulty == "M":
             multiplier = 2
         match random.randint(0, 2):
             case 0:
-                selectedMode = "ADD"
+                num1, num2 = random.randint(0, 10 * multiplier), random.randint(0, 10 * multiplier)
+                questions.append({"q": "What is " + str(num1) + " + " + str(num2) + " ? ", "a": num1 + num2, "s": False,
+                                  "o": selectedMode})
             case 1:
-                selectedMode = "SUB"
+                num1, num2 = random.randint(0, 10 * multiplier), random.randint(0, 10 * multiplier)
+                questions.append({"q": "What is " + str(num1) + " - " + str(num2) + " ? ", "a": num1 - num2, "s": False,
+                                  "o": selectedMode})
             case 2:
-                selectedMode = "MULTI"
-        if selectedMode == "ADD":
-            num1, num2 = random.randint(0, 10 * multiplier), random.randint(0, 10 * multiplier)
-            questions.append({"q": "What is " + str(num1) + " + " + str(num2) + " ? ", "a": num1 + num2, "s": False,
-                              "o": selectedMode})
-        elif selectedMode == "SUB":
-            num1, num2 = random.randint(0, 10 * multiplier), random.randint(0, 10 * multiplier)
-            questions.append({"q": "What is " + str(num1) + " - " + str(num2) + " ? ", "a": num1 + num2, "s": False,
-                              "o": selectedMode})
-        elif selectedMode == "MULTI":
-            num1, num2 = (random.randint(0, 10 * math.floor(multiplier - 0.3 * multiplier)),
-                          random.randint(0, 10 * math.floor(multiplier - 0.3 * multiplier)))
-            questions.append({"q": "What is " + str(num1) + " * " + str(num2) + " ? ", "a": num1 + num2, "s": False,
-                              "o": selectedMode})
+                num1, num2 = (random.randint(0, 10 * round(multiplier - 0.3 * multiplier)),
+                              random.randint(0, 10 * round(multiplier - 0.3 * multiplier)))
+                questions.append({"q": "What is " + str(num1) + " * " + str(num2) + " ? ", "a": num1 * num2, "s": False,
+                                  "o": selectedMode})
 
-    print("--- WHEN YOU ARE DONE, TYPE 'DONE' IN ANY QUESTION PROMPT ---")
+    # Question displayer
+    is2Player = False
+    qAnswered1 = 0
+    qAnswered2 = 0
+    qCorrect1 = 0
+    qCorrect2 = 0
     for i in range(len(questions)):
         attempt = 3
         if continuousMode == -2:
             break
         while True:
             try:
-                if attempt == 0:
-                    print("Looks like you don't know this one. Lets move on. \n")
-                    break
-                anw = input(questions[i]["q"])
-                if anw.upper() == "DONE":
-                    continuousMode = -2
-                    break
-                elif int(anw) == questions[i]["a"]:
-                    questions[i]["s"] = True
-                    break
+                if not p2Mode:
+                    # Normal mode
+                    if attempt == 0:
+                        print("Looks like you don't know this one. Lets move on. \n")
+                        qAnswered += 1
+                        break
+                    anw = input(questions[i]["q"])
+                    if anw.upper() == "DONE" and continuousMode == 1:
+                        continuousMode = -2
+                        break
+                    elif int(anw) == questions[i]["a"]:
+                        questions[i]["s"] = True
+                        qAnswered += 1
+                        break
+                    else:
+                        print("Not correct. Try again!")
+                        attempt -= 1
                 else:
-                    print("Not correct. Try again!")
-                    attempt -= 1
+                    # 2 players
+                    if is2Player:
+                        print("Go player 2!")
+                    else:
+                        print("You're up player 1")
+                    if attempt == 0:
+                        print("Looks like you don't know this one. Lets move on. \n")
+                        if is2Player:
+                            qAnswered2 += 1
+                        else:
+                            qAnswered1 += 1
+                        break
+                    anw = input(questions[i]["q"])
+                    if anw.upper() == "DONE":
+                        continuousMode = -2
+                        break
+                    elif int(anw) == questions[i]["a"]:
+                        questions[i]["s"] = True
+                        if is2Player:
+                            qAnswered2 += 1
+                            qCorrect2 += 1
+                        else:
+                            qAnswered1 += 1
+                            qCorrect1 += 1
+                        break
+                    else:
+                        print("Not correct. Try again!")
+                        attempt -= 1
+
             except ValueError:
                 print("Invalid Number. Try again.\n")
+        is2Player = not is2Player
 
+    # Prints the score to the user
+    if p2Mode:
+        print(qCorrect1, qAnswered1, qCorrect2, qAnswered2)
+        print("Player 1 score: " + str(qCorrect1 / qAnswered1 * 100) + "%")
+        print("Player 2 score: " + str(qCorrect2 / qAnswered2 * 100) + "%")
+        if qCorrect1 / qAnswered1 * 100 == qCorrect2 / qAnswered2 * 100:
+            print("Tie!")
+        elif qCorrect1 / qAnswered1 * 100 > qCorrect2 / qAnswered2 * 100:
+            print("Player 1 wins!")
+        else:
+            print("Player 2 wins!")
+        break
     correct = 0
-    for i in range(len(questions)):
+    for i in range(qAnswered):
         if questions[i]["s"]:
             correct += 1
-        if i == len(questions) - 1:
-            print("DONE! \n" + str(correct / amtOfQuestions * 100) + "%")
+    print("DONE! \n" + str(correct / qAnswered * 100) + "%")
 
+    # Instant close after score is told. Used for continuousMode only.
     if continuousMode == -2 or continuousMode == 1:
         break
 
+    # Leveling up, making it hard after 5 questions
     if correct / amtOfQuestions * 100 >= 80.0 and selectedDifficulty == "E":
         selectedDifficulty = "M"
         print("Moving on to medium mode!")
