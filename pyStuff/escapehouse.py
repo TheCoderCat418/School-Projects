678186b082ed467a203b6a92da17a09a22155637
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2024-02-04 22:02:26 -0600

    aaa

--- pyStuff/escapehouse.py
+++ pyStuff/escapehouse.py
@@ -1,12 +1,16 @@
 playerHas = []
 playerData = []
 
+objectData = {"coins": {"@NAME": "Coins", "@DESC": "Some coins.", "@HASUSE": False}}
+
 
 def addToInventory(obj):
     playerHas.append(obj)
 
 
 def isInPlayerData(strg):
+    if strg is None or strg == "":
+        return False
     try:
         playerData.index(strg)
         return True
@@ -18,32 +22,122 @@ def addToPlayerData(strg):
     playerData.append(strg)
 
 
+def rMet(arg):
+    main = True
+    for x in arg:
+        s = x.split("-")
+        match s[0].upper():
+            case "IN":
+                if currentRoom["@NAME"] == s[1]:
+                    main = main and True
+                else:
+                    main = main and False
+            case "HAS":
+                try:
+                    playerHas.index(s[0])
+                    main = main and True
+                except ValueError:
+                    main = main and False
+            case "DATA":
+                try:
+                    playerData.index(s[0])
+                    main = main and True
+                except ValueError:
+                    main = main and False
+    return main
+
+
+def setRoom(arrName):
+    global currentRoom
+    currentRoom = eval(arrName + "Data")
+
+
 MainRoomData = {
+    "@NAME": "MainRoom",
     "@INT": {
         "couch": {
+            "@NAME": "Couch",
             "@RESP": [
                 {
                     "@SUCCESS": "You found some coins!",
                     "@FAILED": "You didn't find anything",
-                    "@RUN": (
-                        lambda: addToInventory("Some Coins"),
-                        addToPlayerData("searchedCouch"),
-                    ),
-                    "@CHECK": lambda: isInPlayerData("searchedCouch"),
+                    "@RUN": lambda: addToInventory("coins"),
+                    "@CHECK": "searchedCouch",
                 }
             ],
             "@ACT": ["Search the couch."],
         },
         "tv": {
+            "@NAME": "TV",
             "@RESP": [
-                {"@SAY": "You pushed the button but nothing happens", "@RUN": None}
+                {
+                    "@SUCCESS": "You pushed the button but nothing happens",
+                    "@RUN": None,
+                    "@FAILED": "",
+                    "@CHECK": None,
+                },
+                {
+                    "@SUCCESS": "You fittle with the cables.",
+                    "@RUN": None,
+                    "@FAILED": "",
+                    "@CHECK": None,
+                },
             ],
-            "@ACT": ["Turn on the tv."],
+            "@ACT": ["Turn on the tv.", "Check cables."],
         },
     },
-    "@MOVE": ["Playroom", "Hallway", "Outside"],
+    "@MOVE": {
+        "playroom": {
+            "@NAME": "Playroom",
+            "@REQ": [],
+            "@REF": "playroom",
+        },
+        "hallway": {
+            "@NAME": "Hallway",
+            "@REQ": [],
+            "@REF": "Hallway",
+        },
+        "outside": {
+            "@NAME": "Outside",
+            "@REQ": ["HAS-key"],
+            "@REF": "outside",
+        },
+    },
 }
-
+HallwayData = {
+    "@NAME": "Hallway",
+    "@INT": {
+        "nothing": {
+            "@NAME": "Nothing",
+            "@RESP": [
+                {
+                    "@SUCCESS": "You did nothing",
+                    "@RUN": None,
+                    "@FAILED": "",
+                    "@CHECK": None,
+                },
+            ],
+            "@ACT": ["Do nothing"],
+        },
+    },
+    "@MOVE": {
+        "mainroom": {
+            "@NAME": "MainRoom",
+            "@REQ": [],
+            "@REF": "MainRoom",
+        },
+        "kitchen": {
+            "@NAME": "Kitchen",
+            "@REQ": [],
+            "@REF": "Kitchen",
+        },
+        "outside": {
+            "@NAME": "Bathroom",
+            "@REQ": ["HAS-key"],
+            "@REF": "Bathroom",
+        },
+    },
+}
 
 PlayroomData = {}
 currentRoom = MainRoomData
@@ -88,9 +182,12 @@ def rootOperation():
             pos = inp.find("MOVE") + 5
             for x in currentRoom["@MOVE"]:
                 if inp.find(x.upper(), pos) != -1:
-                    currentRoom = eval(x + "Data")
-                    print("Moving")
-                    moved = True
+                    if rMet(currentRoom["@MOVE"][x]["@REQ"]):
+                        setRoom(currentRoom["@MOVE"][x]["@REF"])
+                        print("Moving")
+                        moved = True
+                    else:
+                        print("You are missing something!")
             if not moved:
                 print("Please try again")
         elif inp.find("INTERACT") != -1:
@@ -106,8 +203,13 @@ def rootOperation():
                         for y in range(len(currentRoom["@INT"][x]["@ACT"])):
                             if inpu == str(y):
                                 interacted = True
-                                if currentRoom["@INT"][x]["@RESP"][y]["@CHECK"]():
-                                    currentRoom["@INT"][x]["@RESP"][y]["@RUN"]()
+                                if not isInPlayerData(
+                                    currentRoom["@INT"][x]["@RESP"][y]["@CHECK"]
+                                ):
+                                    currentRoom["@INT"][x]["@RESP"][y]["@RUN"]
+                                    addToPlayerData(
+                                        currentRoom["@INT"][x]["@RESP"][y]["@CHECK"]
+                                    )
                                     print(
                                         currentRoom["@INT"][x]["@RESP"][y]["@SUCCESS"]
                                     )
@@ -116,9 +218,36 @@ def rootOperation():
                                 break
                         if not interacted:
                             print("That is not a valid input, please try again.")
-                else:
-                    #fix
-                    print("Object was not found.")
+                    break
+            if not interacted:
+                print("Object was not found.")
+        elif inp.find("USE") != -1:
+            pos = inp.find("USE") + 4
+            used = False
+            for x in playerHas:
+                if inp.find(x.upper(), pos) != -1:
+                    used = True
+                    if objectData[x]["@HASUSE"]:
+                        objectData[x]["@USE"]
+                        print(objectData[x]["@SUCCESS"])
+                        break
+                    else:
+                        print("You can't do anything with this.")
+                        break
+            if not used:
+                print("Object not found.")
+        elif inp.find("LIST") != -1:
+            print("You are in the " + currentRoom["@NAME"] + ".")
+            print("There are " + str(len(currentRoom["@INT"])) + " objects.")
+            for x in currentRoom["@INT"]:
+                print(" - " + currentRoom["@INT"][x]["@NAME"])
+            print("You can move to the following rooms:")
+            for x in currentRoom["@MOVE"]:
+                print(" - " + x)
+            print("You have the following items in your inventory:")
+            for x in playerHas:
+                print(" - " + objectData[x]["@NAME"])
+            print("That is all.")
 
 
 rootOperation()
